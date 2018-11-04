a4192b2114ee58662ad0215ad9abb7f2c04a09e5
Ryan Wiemer <ryan@ryanwiemer.com>
2018-11-04 11:32:01 -0800

    tweaks to login screen

--- src/components/Login/Select.js
+++ src/components/Login/Select.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
+import arrows from '../../images/arrows.svg'
 
 const LanguageSelect = styled.select`
+  display: inline-block;
+  text-align: center;
   position: relative;
-  background: ${props => props.theme.colors.primary};
+  background: #eee url(${arrows}) no-repeat 97% 0 !important;
   border-radius: 3px;
-  padding: 0.25em 0.75em 0.25em 0.5em;
-  color: white;
+  padding: 0.25em 4em 0.25em 0.5em;
+  color: ${props => props.theme.colors.primary};
   cursor: pointer;
   margin: 0 1rem 0 0;
-  &:after {
-  }
 `
 
 const Select = props => {
